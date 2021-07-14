f59e6ccf2a85317a4d3e7e13fc9bf5c6a70b8dac
Marshall <mzmarshall1278@gmail.com>
2021-07-14 20:39:58 +0100

    Typeorm initialized and auth module initialized

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule],
   controllers: [],
   providers: [],
 })
---
+++ src/auth/user.entity.ts
@@ -0,0 +1,26 @@
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+@Entity()
+@Unique(['username'])
+export class User extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+
+  @Column()
+  salt: string;
+
+
+  async validatePassword(password: string):Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    // console.log(hash ===this.password)
+    return hash === this.password;
+  }
+}
