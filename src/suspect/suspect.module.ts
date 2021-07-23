03f6db7e5261c6985a30924583327c2d6d18254e
Marshall <mzmarshall1278@gmail.com>
2021-07-23 17:14:54 +0100

    fix bug: imported auth module in suspect module for authGuards

--- src/suspect/suspect.module.ts
+++ src/suspect/suspect.module.ts
@@ -3,10 +3,12 @@ import { SuspectService } from './suspect.service';
 import { SuspectController } from './suspect.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SuspectRepository } from './suspect.repository';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([SuspectRepository])
+    TypeOrmModule.forFeature([SuspectRepository]),
+    AuthModule
   ],
   providers: [SuspectService],
   controllers: [SuspectController]
